afb5ae88e6cddd64c1b1c4de147ace0469845dca
jpgits <escgot@icloud.com>
2024-08-30 16:01:03 +0900

    comments

---
+++ lib/realworld/blogs/article.ex
@@ -0,0 +1,19 @@
+defmodule Realworld.Blogs.Article do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Realworld.Blogs.Comment
+
+  schema "articles" do
+    field :title, :string
+    field :body, :string
+    has_many :comments, Comment, on_delete: :delete_all
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(article, attrs) do
+    article
+    |> cast(attrs, [:title, :body])
+    |> validate_required([:title, :body])
+  end
+end
---
+++ test/support/fixtures/blogs_fixtures.ex
@@ -0,0 +1,36 @@
+defmodule Realworld.BlogsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Realworld.Blogs` context.
+  """
+
+  @doc """
+  Generate a article.
+  """
+  def article_fixture(attrs \\ %{}) do
+    {:ok, article} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        title: "some title",
+      })
+      |> Realworld.Blogs.create_article()
+
+    article
+  end
+
+  @doc """
+  Generate a comment.
+  """
+  def comment_fixture(attrs \\ %{}) do
+    {:ok, comment} =
+      attrs
+      |> Enum.into(%{
+        body: "some body",
+        article_id: article_fixture().id
+      })
+      |> Realworld.Blogs.create_comment()
+
+    comment
+  end
+end
